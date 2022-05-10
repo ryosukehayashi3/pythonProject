0fe0b9aaa52f61960e097d756ada8ee0bc7db11b
ryosukehayashi3 <ryosuke.810.109@gmail>
2022-05-10 14:33:47 +0900

    Initial commit

---
+++ lesson2.py
@@ -0,0 +1,38 @@
+print("hello")
+print("I don't know")
+print('say "I don\'t know"')
+
+print("hello. \nHow are you?")
+print(r'C:\name\name')
+
+print("##########")
+print("""\
+line1
+line2
+line3\
+""")
+print("##########")
+
+print('Hi.' * 3 + 'Mike')
+print('Py''thon')
+s = 'aaaaaaaaaaaaaaaa'\
+    'bbbbbbbbbbbbbbbb'
+print(s)
+
+
+word = 'python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print(word[0:2])
+print('###################')
+print(word[2:5])
+print(word[:2])
+print('###################')
+print(word[2:])
+word = 'j' + word[1:]
+print(word)
+print('###################')
+print(word[:])
+n = len(word)
+print(n)
---
+++ lesson4.py
@@ -0,0 +1,10 @@
+print("和を求める")
+print("一つ目")
+x1 = input(">")
+print("二つ目")
+y1 = input(">")
+
+x2 = int(x1)
+y2 = int(y1)
+z = x2 + y2
+print("数の和：", z)
