46f5a582ed14bdccab97b400d8c7073bace98fce
ryosukehayashi3 <ryosuke.810.109@gmail>
2022-05-10 15:18:27 +0900

    Changes

--- Lesson_Python/lesson4.py
+++ Lesson_Python/lesson4.py
@@ -2,9 +2,13 @@ print("和を求める")
 print("一つ目")
 x1 = input(">")
 print("二つ目")
-y1 = input(">")
+x2 = input(">")
+print("三つ目")
+x3 = input(">")
 
-x2 = int(x1)
-y2 = int(y1)
-z = x2 + y2
+y1 = int(x1)
+y2 = int(x2)
+y3 = int(x3)
+
+z = y1 + y2 + y3
 print("数の和：", z)
